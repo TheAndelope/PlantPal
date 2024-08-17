59caa0fac092e4a74555341eafe126bff8e865b0
TheAndelope <164644527+TheAndelope@users.noreply.github.com>
2024-08-17 11:12:33 -0400

    Updated api call

--- plantAI.py
+++ plantAI.py
@@ -11,7 +11,7 @@ openai.api_key = api_key
 
 def generate_text(model="gpt-4o-mini", max_tokens=40):
     messages = [{"role": "system", "content": "You are a Plant"}]
-    messages.append({"role": "user", "content": "I just watered you, tell me a joke"})
+    messages.append({"role": "user", "content": "I just watered you, tell me a joke (it doesn't have to be plant related)"})
 
     response = openai.chat.completions.create(
         model=model,
@@ -20,8 +20,7 @@ def generate_text(model="gpt-4o-mini", max_tokens=40):
         temperature=0.7,  # Controls the randomness of the output
         n=1,              # Number of responses to generate
         stop=None,         # Stop sequence, if any
-        stream=False
+        stream=False,
+        frequency_penalty = 2.0
     )
     return response.choices[0].message.content
-
-
